11dad76150f35b4bdd83e4880c508893d0d7a4f0
unknown <cezary.wrobel.1991@gmail.com>
2023-10-29 18:29:53 +0100

    fix

--- test-data/test-data.ts
+++ test-data/test-data.ts
@@ -1,32 +1,32 @@
 export const loginScenario = [
   {
     name: "correct credentials",
-    user: process.env.USER ?? "",
-    pass: process.env.PASSWORD ?? "",
+    user: process.env.USER ?? "standard_user",
+    pass: process.env.PASSWORD ?? "secret_sauce",
     check: "https://www.saucedemo.com/inventory.html",
   },
   {
     name: "incorrect user",
     user: "thisUserIsIncorrect",
-    pass: process.env.PASSWORD ?? "",
+    pass: process.env.PASSWORD ?? "secret_sauce",
     check:
       "Epic sadface: Username and password do not match any user in this service",
   },
   {
     name: "empty password",
-    user: process.env.USER ?? "",
+    user: process.env.USER ?? "standard_user",
     pass: "",
     check: "Epic sadface: Password is required",
   },
   {
     name: "empty user",
     user: "",
-    pass: process.env.PASSWORD ?? "",
+    pass: process.env.PASSWORD ?? "secret_sauce",
     check: "Epic sadface: Username is required",
   },
   {
     name: "incorrect password",
-    user: process.env.USER ?? "",
+    user: process.env.USER ?? "standard_user",
     pass: "incorrect",
     check:
       "Epic sadface: Username and password do not match any user in this service",
